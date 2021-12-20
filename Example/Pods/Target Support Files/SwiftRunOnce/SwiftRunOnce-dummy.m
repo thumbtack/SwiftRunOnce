36659dbaa248ce3264c9fec56b6c85861f7e6afd
Daniel Roth <droth@thumbtack.com>
2021-12-20 12:22:58 -0800

    Release as cocoapod

---
+++ Example/Pods/Target Support Files/SwiftRunOnce/SwiftRunOnce-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftRunOnce : NSObject
+@end
+@implementation PodsDummy_SwiftRunOnce
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,6 @@
 import Foundation
 import XCTest
+import SwiftRunOnce
 
 class RunOnceTest: XCTestCase {
     func testOnlyRunsOnce() {
